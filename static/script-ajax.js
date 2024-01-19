bd7346345f53df85d38f148a592339761ff53c25
olafszyncel <101902323+olafszyncel@users.noreply.github.com>
2024-01-19 10:04:54 +0100

    Add files via upload

---
+++ static/script-ajax.js
@@ -0,0 +1,39 @@
+const button = document.querySelector('#bas');
+
+button.addEventListener('click', event => {
+    fetch('/stripe_pay')
+    .then((result) => { return result.json(); })
+    .then((data) => {
+        var stripe = Stripe(data.checkout_public_key);
+        stripe.redirectToCheckout({
+            // Make the id field from the Checkout Session creation API response
+            // available to this file, so you can provide it as parameter here
+            // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+            sessionId: data.checkout_session_id
+        }).then(function (result) {
+            // If `redirectToCheckout` fails due to a browser or network
+            // error, display the localized error message to your customer
+            // using `result.error.message`.
+        });
+    })
+});
+
+const button2 = document.querySelector('#adv');
+
+button2.addEventListener('click', event => {
+    fetch('/stripe_pay2')
+    .then((result) => { return result.json(); })
+    .then((data) => {
+        var stripe = Stripe(data.checkout_public_key);
+        stripe.redirectToCheckout({
+            // Make the id field from the Checkout Session creation API response
+            // available to this file, so you can provide it as parameter here
+            // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+            sessionId: data.checkout_session_id
+        }).then(function (result) {
+            // If `redirectToCheckout` fails due to a browser or network
+            // error, display the localized error message to your customer
+            // using `result.error.message`.
+        });
+    })
+});
---
+++ static/script-no-ajax.js
@@ -0,0 +1,31 @@
+var stripe = Stripe(checkout_public_key);
+
+const button = document.querySelector('#bas');
+
+button.addEventListener('click', event => {
+    stripe.redirectToCheckout({
+        // Make the id field from the Checkout Session creation API response
+        // available to this file, so you can provide it as parameter here
+        // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+        sessionId: checkout_session_id
+    }).then(function (result) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer
+        // using `result.error.message`.
+    });
+})
+
+const button2 = document.querySelector('#adv');
+
+button2.addEventListener('click', event => {
+    stripe.redirectToCheckout({
+        // Make the id field from the Checkout Session creation API response
+        // available to this file, so you can provide it as parameter here
+        // instead of the {{CHECKOUT_SESSION_ID}} placeholder.
+        sessionId: checkout_session_id
+    }).then(function (result) {
+        // If `redirectToCheckout` fails due to a browser or network
+        // error, display the localized error message to your customer
+        // using `result.error.message`.
+    });
+})
